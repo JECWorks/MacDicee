44899e7b9a95c951be935d08043e03c7a3342f65
Jason Cox <jasoncox@mac.com>
2020-06-16 21:01:44 -0400

    Initial commit, pretty functional 2-dice random dice generator!

--- MacDicee/ViewController.swift
+++ MacDicee/ViewController.swift
@@ -10,10 +10,19 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet var imageViewDice1: NSImageView!
+    @IBOutlet var imageViewDice2: NSImageView!
+    @IBOutlet var imageViewTitle: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        // imageViewTitle.image = NSImage(named: "diceeLogo")
     }
 
     override var representedObject: Any? {
@@ -22,6 +31,15 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func buttonRoll(_ sender: Any) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1, randomDiceIndex2)
+        
+        imageViewDice1.image = NSImage(named: diceArray[randomDiceIndex1])
+        imageViewDice2.image = NSImage(named: diceArray[randomDiceIndex2])
+    }
+    
 }
 
